4aa6a0f013beec9d4e2418de552f9387045df821
AliGorithm <alisher.coder@gmail.com>
2017-12-25 20:27:41 -0600

    first commit

---
+++ SimpleIntroductionToPY.py
@@ -0,0 +1,26 @@
+print('Hello World')
+print(1 + 2)
+print(7 * 9)
+print()
+print("The End")
+print("Python's strings are easy to use")
+print("Hello World")
+print("hello" + ' world')
+greeting = "Hello"
+name = "Bruce"
+print(greeting + name)
+# if we want to leave a comment about our code we use hashtag sign #
+print(greeting + " " + name)
+# using prompt with input
+name = input("Please provide your name: ")
+print(greeting + " " + name)
+
+# Several lines String
+splitStringWithThreeQuotes= ''' He said "I am not hungary...
+but that he still would like to eat anyways.
+lol
+'''
+
+splitStringWithThreeDoubleQuotes = """  He said "I am not hungary..."
+but that he still would like to eat anyways.
+lol """
---
+++ Variables.py
@@ -0,0 +1,11 @@
+# When program runs everything will get to stored somewhere in the memory
+# When we create a variable by giving a name for instance fruit="apple"
+# memory allocates storage for it
+# we can name variables like: greeting = "Hello", _myage = 26, ali_job_title = "software engineer"
+
+
+# converting in python
+age = 25 # whole number Integer
+name = "Alisher"
+salary = 50.550 # Float number
+print(name + " is " + str(age) + " years old.")
